41d36e63d0d8bbba6cc0ac099c6b630b85fb8940
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-07-21 18:52:53 +0200

    added static file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -27,6 +28,8 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
 
 morgan.token('body', (res) => JSON.stringify(res.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -101,7 +104,7 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
