69699f19a7f7a7b1b1884e26adc25e8e24803ba0
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-07-23 00:43:14 +0200

    3.20 prnding refine regex

--- models/contact.js
+++ models/contact.js
@@ -19,7 +19,13 @@ const contactSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        minLength: 5,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+              return /\d{2,3}-\d+/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+          },
         required: true,
     },
 })
