434e3b5ee5ba0569572aa78c88c2d0a612689e27
iyustlop <iyustlop@gmail.com>
2024-07-23 16:46:49 +0200

    3.21 fix regex and deploy

--- models/contact.js
+++ models/contact.js
@@ -22,7 +22,7 @@ const contactSchema = new mongoose.Schema({
         minLength: 8,
         validate: {
             validator: function(v) {
-              return /\d{2,3}-\d{5,}/.test(v);
+              return /^\d{2,3}-\d{5,}/.test(v);
             },
             message: props => `${props.value} is not a valid phone number!`
           },
