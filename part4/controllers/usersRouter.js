860e752f1c194d013d379f3f5d234afc59957a88
Ignacio Yuste Lopez <iyustlop@gmail.com>
2024-07-30 00:16:24 +0200

    4.17 populate

--- part4/controllers/usersRouter.js
+++ part4/controllers/usersRouter.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const Blog = require('../models/blogs')
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
@@ -25,6 +26,7 @@ usersRouter.post('/', async (request, response) => {
 usersRouter.get('/', async (request, response) => {
   const users = await User
     .find({})
+    .populate('blogs', { url: 1, title: 1, author:1 })
 
   response.json(users)
 })
